ea262aaefaf8fa9bc84fa116176a800130307dad
Al Momin <al.momin@outlook.com>
2017-05-02 21:28:08 -0400

    Add a conjecture, save some simulation runs.

--- 2017-04-28/simulation.py
+++ 2017-04-28/simulation.py
@@ -21,13 +21,13 @@ def play(balls, debug=False):
         c += 1
     return c
 
-def simulation(n_balls, N_runs, debug=False):
+def simulation(balls, N_runs, debug=False):
     total = 0.0
     for i in range(0,N_runs):
-        balls = range(0,n_balls)
-        c = play(balls, debug)
+        initial_balls = list(balls)
+        c = play(initial_balls, debug)
         total += c
-    print total / N_runs
+    return total / N_runs
 
 def state(balls):
     x = [0]*4
@@ -70,7 +70,61 @@ def transitions():
         s = sum(row)
         print map(lambda(y): y/s, row)
 
-n_balls = 4
-N_runs = 50000
-debug=False
-simulation(n_balls, N_runs, debug)
+def balls_from_partition(partition):
+    i = 0
+    balls = []
+    for p in partition:
+        for j in range(0, p):
+            balls.append(i)
+        i += 1
+    return balls
+
+def partitions(n):
+    return partitions_up_to(n, n)
+
+def partitions_up_to(n, max_p):
+    if max_p > n: return partitions_up_to(n, n)
+    results = []
+    if max_p >= n: results = [[n]]
+    for i in range(max_p, 0, -1):
+        remaining = n - i
+        new_max_p = min(max_p, min(i, n-i))
+        if new_max_p < 1: continue
+        sub_results = map(lambda(x): [i] + x, partitions_up_to(remaining, new_max_p))
+        results += sub_results
+    return results
+        
+
+#print partitions(2)
+#print partitions(3)
+#print partitions(4)
+#print partitions(5)
+#print partitions(6)
+
+def simulate_all_partitions_of(n, N_runs):
+    for p in partitions(n):
+        ball_distribution = balls_from_partition(p)
+        debug=False
+        turns = simulation(ball_distribution, N_runs, debug)
+        print ball_distribution, p, "->", turns
+
+def simulate_two_colors_with_n_balls(N, N_runs):
+    for i in range(0,N):
+        if i > N/2: break
+        p = [i,N-i]
+        ball_distribution = balls_from_partition(p)
+        debug=False
+        turns = simulation(ball_distribution, N_runs, debug)
+        print ball_distribution, p, "->", turns
+
+def simulate_all_red_but_one_up_to(from_, to_, N_runs):
+    for i in range(from_, to_ + 1):
+        p = [i,1]
+        ball_distribution = balls_from_partition(p)
+        debug=False
+        turns = simulation(ball_distribution, N_runs, debug)
+        print ball_distribution, p, "->", turns
+
+simulate_all_partitions_of(8, 500000)
+#simulate_two_colors_with_n_balls(6, 100000)
+#simulate_all_red_but_one_up_to(5, 5, 10000000)
